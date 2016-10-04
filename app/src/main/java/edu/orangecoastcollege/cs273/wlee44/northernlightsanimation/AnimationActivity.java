f998871ffe6f0f33a785ad2eda7614b1e7065035
Wonseop <wonseop.s.lee@gmail.com>
2016-10-04 16:56:29 -0700

    endofclass commit

--- app/src/main/java/edu/orangecoastcollege/cs273/wlee44/northernlightsanimation/AnimationActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/wlee44/northernlightsanimation/AnimationActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class AnimationActivity extends AppCompatActivity {
@@ -31,8 +32,28 @@ public class AnimationActivity extends AppCompatActivity {
         // 2) Associate the frameAnim with the animation in XML
         frameAnim = (AnimationDrawable) lightsImageView.getBackground();
         // 3) Start the frame animation
-        frameAnim.start();
+        if (frameAnim.isRunning())
+            frameAnim.stop();
+        else
+            frameAnim.start();
     }
 
+    public void toggleRotateAnim(View view)
+    {
+        if (rotateAnim!= null && rotateAnim.hasStarted()) {
+            lightsImageView.clearAnimation();
+            rotateAnim = null;
+        }
+        else {
+            rotateAnim = AnimationUtils.loadAnimation(this, R.anim.rotate_anim);
+            lightsImageView.startAnimation(rotateAnim);
+        }
+    }
 
+    public void toggleShakeAnim(View view)
+    {
+        shakeAnim = AnimationUtils.loadAnimation(this, R.anim.shake_anim);
+        lightsImageView.startAnimation(shakeAnim);
+
+    }
 }
